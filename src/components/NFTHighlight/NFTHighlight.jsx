5edbdf27c5eae4f52737a01f87ff511880e5f8b4
Shaikh Wasim <shaikhwasim.stc@gmail.com>
2025-08-28 16:35:09 +0530

    Fixed some changes in Highlight section

--- src/components/NFTHighlight/NFTHighlight.jsx
+++ src/components/NFTHighlight/NFTHighlight.jsx
@@ -66,15 +66,66 @@
 // export default NFTHighlight;
 import React from "react";
 import "./NFTHighlight.css";
+import { FaEye } from "react-icons/fa";
 
 const NFTHighlight = () => {
   return (
-    <section className="highlight">
-      <img
-        src="/assets/nfts/nft-highlight.png"
-        alt="NFT Highlight"
-        className="highlight-image"
-      />
+    <section className="nft-highlight-container">
+      {/* Layered Background */}
+      <div className="nft-highlight-bg">
+        <img
+          src="/assets/mushrooms-bg.png"
+          alt="Magic Mushrooms"
+          className="highlight-bg-img"
+        />
+        <div className="highlight-bg-gradient"></div>
+      </div>
+
+      {/* Main Content */}
+      <div className="nft-highlight-content">
+        {/* Left: Artist, Title, Button */}
+        <div className="highlight-left">
+          {/* Artist Card */}
+          <div className="highlight-artist-card">
+            <img
+              src="/assets/avatar.png"
+              alt="Dish Studio"
+              className="artist-avatar"
+            />
+            <span className="artist-name">Dish Studio</span>
+          </div>
+
+          {/* Title */}
+          <h2 className="highlight-title">Magic Mushrooms</h2>
+
+          {/* Button */}
+          <button className="highlight-btn">
+            <FaEye className="btn-icon" />
+            <span>See NFT</span>
+          </button>
+        </div>
+
+        {/* Right: Timer */}
+        <div className="highlight-timer-card">
+          <p className="timer-label">Auction ends in:</p>
+          <div className="timer-values">
+            <div className="timer-box">
+              <h3>59</h3>
+              <span>Hours</span>
+            </div>
+            <span className="timer-colon">:</span>
+            <div className="timer-box">
+              <h3>59</h3>
+              <span>Minutes</span>
+            </div>
+            <span className="timer-colon">:</span>
+            <div className="timer-box">
+              <h3>59</h3>
+              <span>Seconds</span>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
